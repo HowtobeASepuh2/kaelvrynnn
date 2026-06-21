2336397b0c0d587c6295ae77a51551a8b679999b
Wisnucuy <wisnujrngr@gmail.com>
2026-06-21 09:38:06 +0700

    google search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="EpxdVnoMagYQoXNjaLKTKdFTlZ98fcehcmzmpsWl9r8" />
 
     {{-- Dynamic Title --}}
     <title>@yield('title', 'Wisnu Nugroho | UI/UX Designer & Creative Editor')</title>
